c1dd0cea050e865b8cad9d8ce4fc56c71a83ddfe
erkansevm <erkansevm528@gmail.com>
2022-02-11 01:21:45 +0300

    Initial Commit

---
+++ AgeOfEmpires2Wiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AgeOfEmpires2Wiki
+//
+//  Created by Erkan Sevim on 11.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
